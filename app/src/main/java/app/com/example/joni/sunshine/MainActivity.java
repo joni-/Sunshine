69426aca5f4ac63b8b2c0295eeaa1ff9fafc46d6
Joni Nevalainen <joninevalainen@gmail.com>
2014-08-20 20:19:27 +0300

    Download weather data from the internet

--- app/src/main/java/app/com/example/joni/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/joni/sunshine/MainActivity.java
@@ -1,16 +1,31 @@
 package app.com.example.joni.sunshine;
 
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -51,6 +66,8 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private static String TAG = "PlaceholderFragment";
+
         public PlaceholderFragment() {
         }
 
@@ -58,7 +75,74 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> forecastData = getForecastData();
+            ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview, forecastData);
+            ListView forecastList = (ListView) rootView.findViewById(R.id.listview_forecast);
+            forecastList.setAdapter(adapter);
+
             return rootView;
+        }
+
+        private List<String> getForecastData() {
+            String jsonForecast = readForecastData();
+            List<String> forecastData = parseForecastData(jsonForecast);
+            return forecastData;
+        }
+
+        private String readForecastData() {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonResponse = null;
+
+            try {
+                String link = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Joensuu&mode=json&units=metric&cnt=7";
+
+                // TODO: Remove this hack and create ASyncTask for downloading the weather data
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+
+                URL url = new URL(link);
+
+                Log.i(PlaceholderFragment.TAG, String.format("Reading data from %s", link));
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    jsonResponse = null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                jsonResponse = buffer.toString();
+            } catch (IOException e) {
+                Log.e(PlaceholderFragment.TAG, "Error while reading data from Open Weather Map", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (Exception e) {
+                        Log.e(PlaceholderFragment.TAG, "Error closing BufferedReader stream", e);
+                    }
+                }
+            }
+
+            return jsonResponse;
+        }
+
+        private List<String> parseForecastData(String jsonForecast) {
+            return new ArrayList<String>();
         }
     }
 }
